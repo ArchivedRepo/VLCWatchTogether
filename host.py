96da560dffe86dd491883a97ac16ab16ceaacc61
will <will.qie@mail.utoronto.ca>
2021-04-29 15:23:30 -0400

    better argument handling

--- host.py
+++ host.py
@@ -15,10 +15,25 @@ class Host:
         while True:
             raw_input = input('Command:')
             args = raw_input.split()
-            command, time_stamp = int(args[0]), int(args[1])
-            command_format = '!HI'
-            packed = struct.pack(command_format, command, time_stamp)
-            s.sendall(packed)
+            try:
+                command = int(args[0])
+                if command != 3:
+                    time_stamp = 0
+                else:
+                    raw_time = args[1]
+                    if ':' not in raw_time:
+                        time_stamp = int(args[1])
+                    else:
+                        this_splited = raw_time.split(":")
+                        if len(this_splited) == 3:
+                            time_stamp = int(this_splited[0]) * 3600 + int(this_splited[1]) * 60 + int(this_splited[2])
+                        else:
+                            time_stamp = int(this_splited[0]) * 60 + int(this_splited[1])
+                command_format = '!HI'
+                packed = struct.pack(command_format, command, time_stamp)
+                s.sendall(packed)
+            except Exception as e:
+                logging.error(f"Error in parsing input or sending message, exception {e}")
 
 
 if __name__ == "__main__":
